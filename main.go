af2e6763af03a57adedf9bc49188dcacf583b412
Artur Rodzaj <Artur.Rodzaj@dreamlab.pl>
2018-12-14 14:23:53 +0100

    init

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	log "github.com/Sirupsen/logrus"
+)
+
+func run() error {
+	return nil
+}
+
 func main() {
+	log.Info("Starting script")
+	if err := run(); err != nil {
+		log.Panic("...")
+	}
 
 }
