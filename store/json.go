80f693197c0e9065436e57c3adc961f78b7c4833
Artur Rodzaj <Artur.Rodzaj@zoho.com>
2018-12-18 01:29:29 +0100

    New element interface

--- store/json.go
+++ store/json.go
@@ -6,3 +6,18 @@ type json struct {
 func (j *json) String() string {
 	return ""
 }
+
+type Element interface {
+	String() string
+	Parent() *Element
+	Value() string
+	Type() string // leaf, array, object
+
+	Childs() map[string]Element
+}
+
+// type Element struct {
+// 	elType int
+// 	value  string
+// 	level  int
+// }
